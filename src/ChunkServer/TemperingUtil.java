63e5d43cca4ecca37ba19f3e3ab70a7f4b4ec992
Alpesh  Parekh <aparekh@ptc.com>
2017-11-06 22:11:25 +0530

    generate sha1id from byte array.

--- src/ChunkServer/TemperingUtil.java
+++ src/ChunkServer/TemperingUtil.java
@@ -67,4 +67,20 @@ public class TemperingUtil {
 		System.out.println("Digest(in hex format):: " + sb.toString());
 
 	}
+
+
+   public static String generateChecksumOfAllBytes(byte[] data) throws Exception {
+      
+      MessageDigest md = MessageDigest.getInstance("SHA1");
+      md.update(data);
+      byte[] mdbytes = md.digest();
+
+      // convert the byte to hex format
+      StringBuffer sb = new StringBuffer("");
+      for (int i = 0; i < mdbytes.length; i++) {
+           sb.append(Integer.toString((mdbytes[i] & 0xff) + 0x100, 16).substring(1));
+      }
+      
+      return sb.toString();
+   }
 }
